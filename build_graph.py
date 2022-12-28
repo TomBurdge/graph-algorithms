dbae44a071ae7ae443a8bf3274d73bf521ba43d3
TomBurdge <tomburdge3@gmail.com>
2022-12-28 15:55:01 +0000

    'add-build_graph.py'

---
+++ build_graph.py
@@ -0,0 +1,13 @@
+def buildgraph(edges, undirected=False):
+    graph = {}
+    
+    # Generate a dictionary with all the nodes
+    [graph.update({edge_1:[]}) for edge_1,edge_2 in edges]
+    [graph.update({edge_2:[]}) for edge_1,edge_2 in edges]
+    
+    # Add all the edges
+    [graph[edge_1].append(edge_2 )for edge_1,edge_2 in edges]
+    # Make the edges reflexive if it is an undirected graph
+    if undirected==True:
+        [graph[edge_2].append(edge_1)for edge_1,edge_2 in edges]
+    return graph
